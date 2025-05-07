e230c0c3a9a79a5e697a52eb9163a4c078107542
ibrahim <ibrahimjp630@gmail.com>
2025-05-07 09:37:21 +0530

    deploy

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -28,9 +28,6 @@ const Header = () => {
             </div>
             <div className="col-lg-4 col-md-4">
               <ul className="social-links">
-                <li><a href="#"><i className="fab fa-facebook"></i></a></li>
-                <li><a href="https://x.com/minthu" target="_blank"><i className="fab fa-twitter"></i></a></li>
-                <li><a href="#"><i className="fab fa-linkedin"></i></a></li>
                 <li><a href="https://www.instagram.com/abron_trinoxabrasives?igsh=NHVyZGJhaGx4ZXF4&utm_source=qr"><i className="fab fa-instagram"></i></a></li>
               </ul>
             </div>
