6ea2dd89f11bbf11051bcb2194112c6b8dd9acec
ibrahim <ibrahimjp630@gmail.com>
2025-05-12 10:33:57 +0530

    Initial commit

--- src/components/SideCol.jsx
+++ src/components/SideCol.jsx
@@ -5,11 +5,11 @@ const sideCol = () => {
     <div>
         <div className='left_social'>
         <a href="https://wa.me/918885656752" target='_blank' className='text-bold text-green-500'>
-        <i class="fa-brands fa-whatsapp font-bold text-xl text-green-500"></i>
+        <img src="/images/whatsapp-icon-logo-svgrepo-com.svg" alt="" />
          <span>Whatsapp</span>
          </a>
          <a href="https://www.instagram.com/abron_trinoxabrasives?igsh=NHVyZGJhaGx4ZXF4&utm_source=qr" target='_blank' className='text-bold text-purple-500'>
-         <i class="fa-brands fa-instagram text-xl text-purple-700"></i>
+         <img src="/images/instagram-1-svgrepo-com.svg" alt="" />
          <span>Instagram</span>
          </a>
         </div>
